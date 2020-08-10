36ecbdd0997cce22a49945a18f763d85a501f7ab
maSchoeller <git@marvinschoeller.de>
2020-08-10 08:22:54 +0200

    [Dev]: add basic components

--- homag-newbies-paperchase/src/app/app.module.ts
+++ homag-newbies-paperchase/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ScannerComponent } from './home/scanner/scanner.component';
+import { QuestionComponent } from './home/question/question.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScannerComponent,
+    QuestionComponent
   ],
   imports: [
     BrowserModule,
