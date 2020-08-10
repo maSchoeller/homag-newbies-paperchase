0f27ddd1eaee1c2ab657a210ca6baf97fe832eb7
maSchoeller <git@marvinschoeller.de>
2020-08-10 08:39:42 +0200

    [Dev]: improve basic routing and pagenotfound component

--- homag-newbies-paperchase/src/app/app-routing.module.ts
+++ homag-newbies-paperchase/src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ScannerComponent } from './home/scanner/scanner.component';
+import { QuestionComponent } from './home/question/question.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  { },
-  { }
+  { path: '', component: ScannerComponent, pathMatch: 'full' },
+  { path: 'scanner', component: ScannerComponent },
+  { path: 'question/:id', component: QuestionComponent},
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
--- homag-newbies-paperchase/src/app/app.module.ts
+++ homag-newbies-paperchase/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ScannerComponent } from './home/scanner/scanner.component';
 import { QuestionComponent } from './home/question/question.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ScannerComponent,
-    QuestionComponent
+    QuestionComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
