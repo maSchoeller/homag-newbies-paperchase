91d959d13f00a13637dda3fb946bbee8609ca806
maSchoeller <git@marvinschoeller.de>
2020-08-12 08:08:45 +0200

    [Dev]: small improvements

--- homag-newbies-paperchase/src/app/home/question/question.component.ts
+++ homag-newbies-paperchase/src/app/home/question/question.component.ts
@@ -1,26 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { QuestionLoaderService} from '../../services/question-loader.service'
+import { QuestionLoaderService } from '../../services/question-loader.service';
 import { IQuestion, Questiontype } from 'src/app/models/IQuestion';
 
 import INumberQuestion from 'src/app/models/INumberQuestion';
 import INameQuestion from 'src/app/models/INameQuestion';
-import {IMultipleChoiceQuestion, IAnswer} from 'src/app/models/IMultipleChoiceQuestion';
+import {
+  IMultipleChoiceQuestion,
+  IAnswer,
+} from 'src/app/models/IMultipleChoiceQuestion';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogComponent } from '../dialog/dialog.component';
 
 @Component({
   templateUrl: './question.component.html',
-  styleUrls: ['./question.component.scss']
+  styleUrls: ['./question.component.scss'],
 })
 export class QuestionComponent implements OnInit {
-
-  constructor(private route: ActivatedRoute,
-              private router: Router,
-              private questionLoader: QuestionLoaderService,
-              private dialog: MatDialog) {
-               this.nameSelection = '';
-              }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private questionLoader: QuestionLoaderService,
+    private dialog: MatDialog
+  ) {
+    this.nameSelection = '';
+  }
 
   question: IQuestion;
   multipleChoiceSelection: IAnswer;
@@ -30,22 +34,21 @@ export class QuestionComponent implements OnInit {
 
   ngOnInit(): void {
     let routeParams = this.route.snapshot.params;
-    console.log(routeParams.id);
     this.question = this.questionLoader.findQuestion(routeParams.id);
     if (this.question) {
       this.questionFound = true;
     }
   }
 
-  isNumberQuestion(q: IQuestion): q is INumberQuestion{
-    return q.type=== Questiontype.Number;
+  isNumberQuestion(q: IQuestion): q is INumberQuestion {
+    return q.type === Questiontype.Number;
   }
 
-  isNameQuestion(q: IQuestion): q is INameQuestion{
+  isNameQuestion(q: IQuestion): q is INameQuestion {
     return q.type === Questiontype.Name;
   }
 
-  isMultipleChoiceQuestion(q: IQuestion): q is IMultipleChoiceQuestion{
+  isMultipleChoiceQuestion(q: IQuestion): q is IMultipleChoiceQuestion {
     return q.type === Questiontype.MultipleChoice;
   }
 
@@ -53,29 +56,39 @@ export class QuestionComponent implements OnInit {
     let right = false;
     if (this.isMultipleChoiceQuestion(this.question)) {
       console.log(this.multipleChoiceSelection);
-      if (this.multipleChoiceSelection.right) {
-          right = true;
-        }
+      if (this.multipleChoiceSelection?.right) {
+        right = true;
       }
-    else if (this.isNameQuestion(this.question)) {
+    } else if (this.isNameQuestion(this.question)) {
       if (this.question.answer === this.nameSelection) {
         right = true;
       }
-    } else if(this.isNumberQuestion(this.question)) {
-        if (Math.abs(this.question.answer - this.numberSelection) <= (this.question.range*this.question.answer)) {
-          right = true;
-        }
+    } else if (this.isNumberQuestion(this.question)) {
+      if (
+        Math.abs(this.question.answer - this.numberSelection) <=
+        this.question.range * this.question.answer
+      ) {
+        right = true;
+      }
     } else {
-      console.log("Unsupported Question type.");
+      console.log('Unsupported Question type.');
       return;
     }
     if (right) {
-      const dialogRef = this.dialog.open(DialogComponent, { data : { successful: true }});
-      const result = await this.router.navigate(["map",this.question.nextLocation.latitude,this.question.nextLocation.longitude])
+      const dialogRef = this.dialog.open(DialogComponent, {
+        data: { successful: true },
+      });
+      await dialogRef.afterClosed().toPromise();
+      const result = await this.router.navigate([
+        'map',
+        this.question.nextLocation.latitude,
+        this.question.nextLocation.longitude,
+      ]);
       console.log(result);
-    }
-    else {
-      const dialogRef = this.dialog.open(DialogComponent, { data : { successful: false }});
+    } else {
+      const dialogRef = this.dialog.open(DialogComponent, {
+        data: { successful: false },
+      });
     }
   }
 }
